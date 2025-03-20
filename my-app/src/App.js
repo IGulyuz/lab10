b156bbbbec684ff037661a6b0e9ef162db1e3153
Gulyuz <g.ibragimova@newuu.uz>
2025-03-20 23:51:22 +0500

    edit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,8 +15,8 @@ function App() {
         div >
         <
         h1 > React + Spring Boot < /h1> <
-        p > { message } < /p> <
-        /div>
+        p > { message } < /p> < /
+        div >
     );
 }
 
